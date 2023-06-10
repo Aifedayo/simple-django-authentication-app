2069ce0579a92156c63e1796951a1ff8fdc5c3c6
akeemlag <akeemifedayolag@gmail.com>
2023-06-10 18:30:44 +0100

    Update forms file with the needed import

--- django_auth/accounts/forms.py
+++ django_auth/accounts/forms.py
@@ -12,3 +12,24 @@ from django.utils.translation import gettext_lazy as _
 
 class UserCacheMixin:
     user_cache = None
+
+
+class SigIn(UserCacheMixin, forms.Form):
+    password = forms.CharField(label=_('Password'), strip=False, widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs):
+
+        if settings.USE_REMEMBER_ME:
+            self.fields['remember_me'] = forms.BooleanField(label=_('Remember me'), required=False)
+
+    def clean_password(self):
+        password = self.cleaned_data['password']
+
+        if not self.user_cache:
+            return password
+        
+        if not self.user_cache.check_password(password):
+            raise ValidationError(_('You entered an invalid password.'))
+        
+        return password
